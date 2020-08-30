b5328b1f7b5d9cf5247907c6c062c61d11a9162a
chiangmaithai <thomas.simms@gmail.com>
2020-08-30 16:38:54 -0400

    Added eureka registration.

--- src/main/java/com/simms/healthcare/ServiceApplication.java
+++ src/main/java/com/simms/healthcare/ServiceApplication.java
@@ -1,30 +1,17 @@
 package com.simms.healthcare;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import com.simms.healthcare.businesslogic.ActivationStatusCode;
-import com.simms.healthcare.entity.Dependents;
-import com.simms.healthcare.entity.Enrollee;
-import com.simms.healthcare.repository.EnrolleeRepository;
-import com.simms.healthcare.service.EnrolleeService;
-import com.simms.healthcare.util.Util;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Microservice to handle processing of enrollees.
  * @author thoma
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceApplication {
 	
